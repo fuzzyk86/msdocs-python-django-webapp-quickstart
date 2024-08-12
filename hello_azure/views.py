193788c3f10af8eb782557ef41ea3fd612044da9
fuzzyk86 <jesus.benavidesm@gmail.com>
2024-08-12 11:59:39 -0600

    add sentiment analysis to phrase

--- hello_azure/views.py
+++ hello_azure/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from pysentimiento import create_analyzer
+analyzer = create_analyzer(task="sentiment", lang="es")
 
 def index(request):
     print('Request for index page received')
@@ -15,7 +17,11 @@ def hello(request):
             print("Request for hello page received with no name or blank name -- redirecting")
             return redirect('index')
         else:
-            print("Request for hello page received with name=%s" % name)
+            result = analyzer.predict(name)            
+            if result :
+                probas = result['probas']
+                
+            print(f"Result after sentiment analysis: NEGATIVE {probas.NEG} POSITVE {probas.POS} NEUTRAL {probas.NEU}")
             context = {'name': name }
             return render(request, 'hello_azure/hello.html', context)
     else:
