d424bf0051597f90ac2a51babc3b5ff3c0908551
fuzzyk86 <jesus.benavidesm@gmail.com>
2024-08-12 12:50:34 -0600

    fix imports

--- hello_azure/views.py
+++ hello_azure/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-# from pysentimiento import create_analyzer
-# analyzer = create_analyzer(task="sentiment", lang="es")
+from pysentimiento import create_analyzer
+analyzer = create_analyzer(task="sentiment", lang="es")
 
 def index(request):
     print('Request for index page received')
