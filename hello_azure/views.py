643ba80df31bd3ff8b8323579045b427ad0ca312
Jesus E. Benavides Medrano <jesus_fuzzy@hotmail.com>
2024-08-12 20:19:46 -0600

    Update views.py

--- hello_azure/views.py
+++ hello_azure/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from pysentimiento import create_analyzer
-analyzer = create_analyzer(task="sentiment", lang="es")
+#from pysentimiento import create_analyzer
+#analyzer = create_analyzer(task="sentiment", lang="es")
 
 def index(request):
     print('Request for index page received')
@@ -17,11 +17,11 @@ def hello(request):
             print("Request for hello page received with no name or blank name -- redirecting")
             return redirect('index')
         else:
-            result = analyzer.predict(name)            
-            if result :
-                probas = result['probas']
-                name = f"Result after sentiment analysis: NEGATIVE {probas.NEG} POSITVE {probas.POS} NEUTRAL {probas.NEU}"    
-                print(f"Result after sentiment analysis: NEGATIVE {probas.NEG} POSITVE {probas.POS} NEUTRAL {probas.NEU}")
+            #result = analyzer.predict(name)            
+            #if result :
+                #probas = result['probas']
+                #name = f"Result after sentiment analysis: NEGATIVE {probas.NEG} POSITVE {probas.POS} NEUTRAL {probas.NEU}"    
+                #print(f"Result after sentiment analysis: NEGATIVE {probas.NEG} POSITVE {probas.POS} NEUTRAL {probas.NEU}")
             print("Name ==> ", name)
             context = {'name': name }
             return render(request, 'hello_azure/hello.html', context)
